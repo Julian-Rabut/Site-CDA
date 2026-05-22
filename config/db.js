eeb153cdd0c1065911763e4cf9e9487f09e4e4a7
Julian Rabut <rabutjulian@gmail.com>
2026-05-22 11:20:42 +0200

    db

--- config/db.js
+++ config/db.js
@@ -3,11 +3,37 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "agenda_rdv",
+  host:
+    process.env.DB_HOST ||
+    process.env.MYSQLHOST ||
+    process.env.MYSQL_HOST ||
+    "127.0.0.1",
+
+  port: Number(
+    process.env.DB_PORT ||
+    process.env.MYSQLPORT ||
+    process.env.MYSQL_PORT ||
+    3306
+  ),
+
+  user:
+    process.env.DB_USER ||
+    process.env.MYSQLUSER ||
+    process.env.MYSQL_USER ||
+    "root",
+
+  password:
+    process.env.DB_PASSWORD ||
+    process.env.MYSQLPASSWORD ||
+    process.env.MYSQL_PASSWORD ||
+    "",
+
+  database:
+    process.env.DB_NAME ||
+    process.env.MYSQLDATABASE ||
+    process.env.MYSQL_DATABASE ||
+    "agenda_rdv",
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
